33591a6eaa93788ad507fab0329e95d808cab4c8
EngKhalilAlmalouli <engkhalilalmalouli@gmail.com>
2025-01-27 21:49:14 +0300

    2nd commit
    Signed-off-by: EngKhalilAlmalouli <engkhalilalmalouli@gmail.com>

---
+++ src/main/java/EmployeesManagement/EmployeesManagement/department/DepartmentResponse.java
@@ -0,0 +1,40 @@
+package EmployeesManagement.EmployeesManagement.department;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DepartmentResponse {
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/EmployeesManagement/EmployeesManagement/employee/EmployeeRequest.java
@@ -0,0 +1,50 @@
+package EmployeesManagement.EmployeesManagement.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+public class EmployeeRequest {
+    private String name;
+    private String phone;
+    private String email;
+    private Long departmentId;
+
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
